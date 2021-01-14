d93f42fa1dbe55105d0aa58e932af12bd915211b
seifemad1999 <you@example.com>
2021-01-14 14:11:37 +0200

    delete Payment

--- backend/payment/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import paymentSerializer
-from .models import payment
-from django.shortcuts import render, get_object_or_404
-
-class paymentViewSet(viewsets.ModelViewSet):
-    queryset = payment.objects.all()
-    serializer_class = paymentSerializer
